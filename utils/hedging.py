f37a9620114bb81f8c6978679d0a6550e8adc965
Martin Molina <molinafmartin@gmail.com>
2025-11-02 09:02:22 -0500

    Implements vectorization of 'delta_hedge'

--- utils/hedging.py
+++ utils/hedging.py
@@ -12,7 +12,109 @@ def hedge_plot(opt_price, portfolio):
     plt.grid(True)
     plt.show()
 
+def delta_hedge(model, model_type, N_paths, N_steps, K, T, S0, seed, **kwargs):
+    """
+    Delta hedging for a single path (seller's point of view) with stepwise cash discounting. Price and delta are vectorized.
+    
+    Parameters
+    ----------
+    model : instance
+        A model implementing .simulate_paths() and .price_greeks().
+    model_type : str
+        Either 'heston' or 'doubleheston'.
+    K : float
+        Option strike.
+    N_steps : int
+        Number of time steps.
+    T : float
+        Time to maturity.
+    S0 : float
+        Initial stock price.
+    seed : int
+        Random seed.
+    **kwargs : dict
+        Model-specific parameters: 
+        - v0 (Heston) 
+        - v01, v02 (DoubleHeston)
+    """
+    # integration parameters for CF pricing
+    Lphi = 1e-5
+    Uphi = 50
+    dphi = 0.001
+    
+    # 1. simulate one path
+    sim_paths = model.simulate_paths(N_paths=N_paths, N_steps=N_steps, T=T, S0=S0, seed=seed, **kwargs)
 
+    # select paths according to model type
+    if model_type == 'heston':
+        S_path, V_path = sim_paths
+    elif model_type == 'doubleheston':
+        S_path, V1_path, V2_path = sim_paths
+    else:
+        raise ValueError("model_type must be 'heston' or 'doubleheston'")
+
+    dt = T / (N_steps - 1)
+    r = model.r
+
+    # time to maturity
+    tau = (N_steps - 1 - np.arange(N_steps)) * dt
+    tau = np.tile(tau, (N_paths, 1))
+
+    # initialize arrays for recording
+    #opt_price = np.zeros((N_paths, N_steps))
+    #delta = np.zeros((N_paths, N_steps))
+    cash = np.zeros((N_paths, N_steps))
+    portfolio = np.zeros((N_paths, N_steps))
+    liability_T = np.zeros(N_paths)
+    hedging_error = np.zeros(N_paths)
+
+    if model_type == 'heston':
+        greeks = model.price_greeks_vect(Lphi=Lphi, Uphi=Uphi, dphi=dphi, K=K, tau=tau, S=S_path, v=V_path)
+    elif model_type == 'doubleheston':
+        greeks = model.price_greeks_vect(Lphi=Lphi, Uphi=Uphi, dphi=dphi, K=K, tau=tau, S=S_path, v1=V1_path, v2=V2_path)
+    else:
+        raise ValueError("model_type must be 'heston' or 'doubleheston'")
+
+    opt_price = greeks["call_price"]
+    delta = greeks["delta"]
+
+    cash[:,0] = opt_price[:,0] - delta[:,0]*S_path[:,0]
+    portfolio[:,0]=delta[:,0]*S_path[:,0]+ cash[:,0]
+    
+    for t in range(1, N_steps-1): # stop before maturity
+        cash[:,t] = cash[:,t-1] * np.exp(r*dt)
+        delta_change = delta[:,t]-delta[:,t-1]
+        cash[:,t] -= delta_change*S_path[:,t] 
+
+        # Portfolio value after rebalancing
+        portfolio[:,t] = delta[:,t]*S_path[:,t]+cash[:,t]
+
+    # At maturity accrue final interest
+    cash[:,-1] = cash[:,-2]*np.exp(r*dt)
+    portfolio[:,-1] = delta[:,-2]*S_path[:,-1]+cash[:,-1]
+
+    # Compute liability and hedging error
+    liability_T = np.maximum(S_path[:,-1]-K,0)
+    hedging_error = portfolio[:,-1]- liability_T
+
+    # choose which V_path(s) to return
+    if model_type == 'heston':
+        V_paths_return = V_path
+    else:
+        V_paths_return = (V1_path, V2_path)
+
+    print(np.mean(hedging_error), np.std(hedging_error))
+    
+    return {
+        "S_path": S_path,
+        "V_path": V_paths_return,
+        "opt_price": opt_price,
+        "delta": delta,
+        "cash": cash,
+        "portfolio": portfolio,
+        "liability_T": liability_T,
+        "hedging_error": hedging_error,
+    }
 
 def delta_hedge_loops(model, model_type, N_paths, N_steps, K, T, S0, seed, **kwargs):
     """
